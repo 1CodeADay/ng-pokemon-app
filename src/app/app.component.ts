e575756c3f234fba9a94ed14d80510f18d66b494
Rodrigue Danso <rodrig.danso@gmail.com>
2024-11-25 07:32:26 +0100

    Add an event binding
    From template to component and from component to template
    Link between event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,27 @@ import { Pokemon } from './pokemon';
 
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
 
 
   ngOnInit() {
-    console.table(this.pokemonList); 
+    console.table(this.pokemonList);
   }
 
-  selectPokemon(event: MouseEvent) {
-    const index: number = +(event.target as HTMLInputElement).value;
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
 
-    console.log(`The name of  your pokemon is ${this.pokemonList[index].name}`);
+    if (pokemon) {
+      this.pokemonSelected = pokemon;
+      console.log(`The name of  your pokemon is ${pokemon.name}`);
+
+    }
+    else {
+      this.pokemonSelected = pokemon;
+      console.log(`No pokemon found`);
+    }
+
   }
+
 }
