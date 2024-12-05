b4f1fd3ce1cacd7f100a84f84702973b5f02e326
Rodrigue Danso <rodrig.danso@gmail.com>
2024-12-05 07:55:24 +0100

    Create route component and add pokemon detail view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
-
-    if (pokemon) {
-      console.log(`Pokemon selected: ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }
-    else {
-      console.log('Pokemon not found');
-      this.pokemonSelected = pokemon;
-    }
-  }
+export class AppComponent {
 
 }
